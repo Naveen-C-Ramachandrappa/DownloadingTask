c5074298b65320a045773ecfb331a3124f177924
NaveenRam <accessnaveen@gmail.com>
2024-10-15 18:23:48 -0500

    Initial Commit

---
+++ DownloadingTask/DownloadingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadingTaskApp.swift
+//  DownloadingTask
+//
+//  Created by Naveen Ramachandrappa on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DownloadingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
